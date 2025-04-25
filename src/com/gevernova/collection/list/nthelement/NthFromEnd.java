4b5827ae67592fff49689b7cd5f67bcdb8fdd1a1
Disha <dishay166@gmail.com>
2025-04-25 11:03:29 +0530

    add list ans

--- src/com/gevernova/collection/list/nthelement/NthFromEnd.java
+++ src/com/gevernova/collection/list/nthelement/NthFromEnd.java
@@ -1,43 +1,46 @@
 package com.gevernova.collection.list.nthelement;
-import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.ListIterator;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
-public class NthFromEnd {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter space separated characters");
-        String[] input = sc.nextLine().split(" ");
-        LinkedList<String> list = new LinkedList<>(Arrays.asList(input));
-        System.out.println("Enter the position from the last");
-        int pos = sc.nextInt();
-        String result = getNthFromEnd(list, pos);
-        if (result != null) {
-            System.out.println("Nth element from the end is: " + result);
-        } else {
-            System.out.println("Invalid value of N.");
+public class NthFromEnd{
+    public static String findNthFromEnd(List<String> list, int N){
+        Iterator<String> fast = list.iterator();
+        Iterator<String> slow = list.iterator();
+
+        // Move fast N steps ahead
+        for(int i = 0; i < N; i++){
+            if(!fast.hasNext()) return "N is too large!";
+            fast.next();
         }
+        // move both iterators until fast reaches the end
+        while(fast.hasNext()){
+            fast.next();
+            slow.next();
+        }
+        return slow.next(); // slow now points to Nth from end
     }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        List<String> list = new LinkedList<>();
 
-    // Function to find Nth node from end using two-pointer method
-    public static String getNthFromEnd(LinkedList<String> list, int n) {
-        ListIterator<String> first = list.listIterator();
-        ListIterator<String> second = list.listIterator();
-        // Move first pointer n steps ahead
-        for (int i = 0; i < n; i++) {
-            if (first.hasNext()) {
-                first.next();
-            } else {
-                return null; // N is greater than the length of the list
-            }
-        }
-        // Move both pointers until first reaches the end
-        while (first.hasNext()) {
-            first.next();
-            second.next();
+        // input size
+        System.out.print("Enter number of elements : ");
+        int size = sc.nextInt();
+
+        // input elements
+        System.out.println("Enter elements : ");
+        for(int i = 0; i<= size; i++){
+            list.add(sc.nextLine());
         }
 
-        return second.next();  // second now points to the Nth from end
+        // Input N
+        System.out.print("Enter N (position from end): ");
+        int N = sc.nextInt();
+
+        // output
+        String result = findNthFromEnd(list, N);
+        System.out.println("Nth element from the end is : "+result);
     }
 }
